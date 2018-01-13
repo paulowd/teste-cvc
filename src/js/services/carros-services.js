b33af20bd629e1f7c566f47ee575f95d5a866b0b
Paulo Victor <paulo@paulowd.com.br>
2018-01-12 22:13:24 -0200

    Comentários no js

--- src/js/services/carros-services.js
+++ src/js/services/carros-services.js
@@ -1,6 +1,6 @@
 angular.module('carrosServices', ['ngResource'])
 	.factory('carrosResource', function($resource) {
-
+		// Recuro da API de carros
 		return $resource('https://5a550e3ba3ba810012c00956.mockapi.io/api/v1/carros', null, {
 			'query' : { 
 				method: 'GET',
@@ -9,7 +9,7 @@ angular.module('carrosServices', ['ngResource'])
 		});
 	})
 	.factory('estadosResource', function($resource) {
-
+		// Recuro da API de Estados
 		return $resource('https://5a550e3ba3ba810012c00956.mockapi.io/api/v1/estados', null, {
 			'query' : { 
 				method: 'GET',
